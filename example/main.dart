4652c79641b45a005269045e2bb10d9fba804852
Krutikgohel <111895589+Krutikgohel@users.noreply.github.com>
2025-12-30 17:28:59 +0530

    Rename package dart file
    Rename package dart file

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_marquee_plus/marquee_plus.dart';
+import 'package:flutter_marquee_plus/flutter_marquee_plus.dart';
 
 void main() {
   runApp(const MyApp());
